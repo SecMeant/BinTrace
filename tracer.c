2df9ceef69aa7de1ecf93d06d3af36272d269934
SecMeant <secmeant@gmail.com>
2021-11-07 09:20:19 +0100

    Adding detection of binary base load address
    Earlier user had to "guess" where the binary will be loaded even tho
    the ASLR was disabled it was annoying. Now after creating process
    we check where the kernel loaded tracee and offset all the traced
    addresses accordingly

--- tracer.c
+++ tracer.c
@@ -16,13 +16,20 @@
 #include <sys/personality.h>
 
 #define BLOCK_SIZE 4096ull
-#define BLOCK_HIST_ENTRY_COUNT ((BLOCK_SIZE - sizeof(struct trace_block_header_t)) / sizeof(hist_entry_t))
-#define CALC_HIST_SIZE_IN_BLOCK(block_size) (((block_size)- sizeof(struct trace_block_t)) / sizeof(hist_entry_t)) ;
+#define BLOCK_HIST_ENTRY_COUNT ((BLOCK_SIZE - sizeof(struct trace_block_header_t)) / sizeof(struct hist_entry_t))
+#define CALC_HIST_SIZE_IN_BLOCK(block_size) \
+	(((block_size) - sizeof(struct trace_block_t)) / sizeof(struct hist_entry_t)) ;
 
 #define BREAK_OPCODE 0xcc
 #define NOP_OPCODE 0x90
 
-typedef uint8_t hist_entry_t;
+#define MAX_HIT_COUNT 0xffffffffffffffff
+
+struct hist_entry_t
+{
+	uint8_t break_index;
+	size_t hit_count;
+};
 
 struct trace_block_t;
 
@@ -36,7 +43,7 @@ struct trace_block_header_t
 struct trace_block_t
 {
 	struct trace_block_header_t hdr;
-	hist_entry_t hist[0];
+	struct hist_entry_t hist[0];
 };
 
 struct trace_context_t
@@ -63,11 +70,86 @@ struct process_t
 {
 	int pid;
 	int status;
+	const char *path;
+	void *base_load_addr;
 };
+
+static uint64_t proc_maps_parse_begin_range(char *maps_line)
+{
+	char     *p      = maps_line;
+	char     *endptr = NULL;
+	uint64_t  addr   = 0;
+
+	while (1) {
+		if (*p == '\0')
+			return 0;
+
+		if (*p == '-') {
+			*p = '\0';
+			addr = strtoul(maps_line, &endptr, 16);
+
+			// If not whole string matched (parsing error)
+			if (!(*maps_line != '\0' && *endptr == '\0')) {
+				addr = 0;
+			}
+
+			*p = '-';
+
+			break;
+		}
+
+		++p;
+	}
+
+	return addr;
+}
+
+static void* get_proc_load_address(struct process_t proc)
+{
+	FILE   *fmaps;
+	char    maps_path[32];
+	char   *maps_line      = NULL;
+	size_t  maps_line_sz   = 0;
+	void   *ret            = NULL;
+
+	snprintf(maps_path, sizeof(maps_path), "/proc/%i/maps", proc.pid);
+
+	fmaps = fopen(maps_path, "rb");
+
+	if (!fmaps) {
+		fprintf(stderr,
+			"Failed to open procs map file. PID: %i, maps: %s\n",
+			proc.pid, maps_path);
+		exit(1);
+	}
+
+	// Parse /proc/pid/maps format and find load address
+	// Example line from maps:
+	// 55ebb23e0000-55ebb23e2000 r--p 00000000 103:03 6304311                   /usr/bin/cat
+	while (getline(&maps_line, &maps_line_sz, fmaps) != -1) {
+		if (strstr(maps_line, proc.path) == NULL)
+			continue;
+
+		ret = (void*) proc_maps_parse_begin_range(maps_line);
+		break;
+	}
+
+	free(maps_line);
+
+	return ret;
+}
 
 struct process_t create_process(const char *path, char *const argv[])
 {
 	struct process_t proc;
+
+	proc.path = realpath(path, NULL);
+	if (!proc.path) {
+		fprintf(stderr, 
+			"Failed to obtain realpath of requested binary: %s\n",
+			path);
+		exit(1);
+	}
 
 	proc.pid = fork();
 	proc.status = 0;
@@ -89,6 +171,23 @@ struct process_t create_process(const char *path, char *const argv[])
 		exit(1);
 	}
 
+	// Wait for PTRACE_TRACEME and exec so we can set options
+	waitpid(proc.pid, NULL, 0);
+
+	if (ptrace(PTRACE_SETOPTIONS, proc.pid, NULL, PTRACE_O_EXITKILL) == -1) {
+		perror("Warning: Failed to set option PTRACE_O_EXITKILL. "
+			"Tracee will not exit when tracer dies.");
+	}
+
+	proc.base_load_addr = get_proc_load_address(proc);
+
+	if (proc.base_load_addr == NULL) {
+		fprintf(stderr,
+			"Failed to obtain load address of child process: %i\n",
+			proc.pid);
+		exit(1);
+	}
+
 	return proc;
 }
 
@@ -102,14 +201,14 @@ static void dump_hist()
 
 		for (size_t off = 0; off < sample_count; ++off) {
 
-			size_t sampleno = (size_t) tb->hist[off];
+			size_t sampleno = (size_t) tb->hist[off].break_index;
 
 			if (sampleno >= patch_info.size) {
 				fprintf(stderr, "Internal error: sample number outside of patch list range (%lu:%lu)\n", off, sampleno);
 				continue;
 			}
 
-			printf("%p\n", patch_info.entry[sampleno].addr);
+			printf("%p: %zu\n", patch_info.entry[sampleno].addr, tb->hist[off].hit_count);
 		}
 	}
 }
@@ -161,6 +260,29 @@ static void trace_hit_fix_exec(uint64_t ip, struct process_t proc, struct patch_
 	ptrace(PTRACE_POKETEXT, proc.pid, ip, ins);
 }
 
+// TODO: name it better lol
+static bool try_compress_hit(size_t hit_break_index)
+{
+	// If this is the beggining of the block we cannot go back and compress
+	if (trace_ctx.offset == 0)
+		return false;
+
+	// If the break_index of the prebious hit is different then current hit
+	// then we cannot compress
+	struct hist_entry_t * prev_hit =
+		&trace_ctx.current_tb->hist[trace_ctx.offset - 1];
+
+	if ( prev_hit->break_index != hit_break_index)
+		return false;
+
+	// If adding to the prev hit counter would overflow we cannot compress
+	if (prev_hit->hit_count == MAX_HIT_COUNT)
+		return false;
+
+	++prev_hit->hit_count;
+	return true;
+}
+
 static void trace_hit(uint64_t ip, struct process_t proc)
 {
 	size_t sampleno;
@@ -171,14 +293,20 @@ static void trace_hit(uint64_t ip, struct process_t proc)
 	for (size_t i = 0; i < patch_info.size; ++i) {
 		if ((void*) ip == patch_info.entry[i].addr) {
 
-			trace_ctx.current_tb->hist[trace_ctx.offset] = i;
-			++trace_ctx.offset;
+			if (try_compress_hit(i) == false)
+			{
+				trace_ctx.current_tb->hist[trace_ctx.offset].break_index = i;
+				++trace_ctx.current_tb->hist[trace_ctx.offset].hit_count;
+				++trace_ctx.offset;
+			}
 
 			trace_hit_fix_exec(ip, proc, patch_info.entry[i]);
 
 			return;
 		}
 	}
+
+	printf("Missing: %p\n", (void*) ip);
 }
 
 static void apply_patches(struct process_t proc)
@@ -186,6 +314,11 @@ static void apply_patches(struct process_t proc)
 	uint64_t ins;
 
 	for (size_t i = 0; i < patch_info.size; ++i) {
+		// Process is loaded now, so we "fix" the patches by adding
+		// binary load address to all the traced addresses
+		if (patch_info.entry[i].addr != 0)
+			patch_info.entry[i].addr += (uint64_t) proc.base_load_addr;
+
 		ins = ptrace(PTRACE_PEEKTEXT, proc.pid, patch_info.entry[i].addr, NULL);
 
 		// Save opcode that will be patched
@@ -274,11 +407,8 @@ int main(int argc, char **argv)
 		NULL,
 	};
 
-	tracee = create_process("./tracee", tracee_argv);
+	tracee = create_process("./test/tracee", tracee_argv);
 	printf("Created process: %i\n", tracee.pid);
-
-	// Wait for execv
-	waitpid(tracee.pid, NULL, 0);
 
 	apply_patches(tracee);
 
